481605335ad9c0f23088241e02661a6236110541
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-01 17:58:59 +0530

    Update accounts.py

--- services/accounts.py
+++ services/accounts.py
@@ -1,1 +1,15 @@
-# login and session mgmt
+from app.database import db
+
+accounts_col = db.accounts
+
+async def save_account(user_id, session_str, phone, username, is_active=True):
+    await accounts_col.insert_one({
+        "owner_id": user_id,
+        "session_string": session_str,
+        "phone": phone,
+        "username": username,
+        "active": is_active
+    })
+
+async def get_user_accounts(user_id):
+    return await accounts_col.find({"owner_id": user_id}).to_list(length=100)
