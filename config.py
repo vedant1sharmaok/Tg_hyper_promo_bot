61bcb47ebabfbdb2578bf7552e9e01e3d4e5e91b
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-01 17:50:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-# config vars
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
