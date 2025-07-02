9fe59006078557fa950fe851cbd1c44a9f52c310
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-02 20:25:13 +0530

    Update sessions.py

--- utils/sessions.py
+++ utils/sessions.py
@@ -1,1 +1,13 @@
-# session conversion
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+from app.config import API_ID, API_HASH
+
+def string_to_session_file(session_string, file_path):
+    client = TelegramClient(file_path, API_ID, API_HASH, session=StringSession(session_string))
+    client.session.save()
+
+def session_file_to_string(file_path):
+    client = TelegramClient(file_path, API_ID, API_HASH)
+    client.connect()
+    return client.session.save()
+  
