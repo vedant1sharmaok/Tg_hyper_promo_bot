e23aa710d0701062b870224348682ccee92d3697
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-02 17:14:51 +0530

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,5 @@
-# router
+from fastapi import APIRouter
+from .endpoints import campaigns_api
+
+api_router = APIRouter()
+api_router.include_router(campaigns_api, prefix="/campaigns", tags=["Campaigns"])
