f5acc8c257e0f893cbf31f2aec27209f94d3b19f
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-02 17:08:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
+OPENAI_KEY = os.getenv("OPENAI_KEY") or "your-openai-key"
