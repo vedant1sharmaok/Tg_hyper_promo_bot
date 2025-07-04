083901e069801ff91c0dfee624d33e4f488c3429
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-05 01:11:46 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,5 +5,7 @@ mongo_client = AsyncIOMotorClient(MONGO_URI)
 db = mongo_client.tgPromoDB
 
 users_col = db.users
+campaigns_col = db.campaigns
+accounts_col = db.accounts
 logs_col = db.logs
 # MongoDB setup
