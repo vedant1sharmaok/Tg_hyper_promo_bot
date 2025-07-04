3a39600724dd8dc9ba35577a55456033fd467417
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-05 01:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
