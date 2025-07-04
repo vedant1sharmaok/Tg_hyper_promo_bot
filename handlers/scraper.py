1344662de2fb86be42a4fa9bbd3c408c8244b41e
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-05 00:11:23 +0530

    Update scraper.py

--- handlers/scraper.py
+++ handlers/scraper.py
@@ -5,7 +5,7 @@ from services.accounts import get_user_accounts
 
 router = Router()
 
-@router.message(commands("scrape_groups"))
+@router.message(Commands("scrape_groups"))
 async def scrape_groups(msg: types.Message):
     accounts = await get_user_accounts(msg.from_user.id)
     if not accounts:
