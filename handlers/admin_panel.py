fabc71686a36d86cafa9eec05acd89d7bf163758
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-07-05 00:08:21 +0530

    Update admin_panel.py

--- handlers/admin_panel.py
+++ handlers/admin_panel.py
@@ -8,14 +8,14 @@ router = Router()
 def is_admin(user_id):
     return user_id in ADMINS
 
-@router.message(commands("panel"))
+@router.message(Commands("panel"))
 async def panel_main(msg: types.Message):
     if not is_admin(msg.from_user.id):
         return await msg.answer("🚫 You’re not authorized.")
 
     await msg.answer("🛠️ Admin Panel\nUse /stats to view system stats.")
 
-@router.message(commands("stats"))
+@router.message(Commands("stats"))
 async def panel_stats(msg: types.Message):
     if not is_admin(msg.from_user.id):
         return await msg.answer("🚫 You’re not authorized.")
